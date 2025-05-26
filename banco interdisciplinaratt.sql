3a2935b8f0d48387bb968be5563a82e3bca13501
lucasfreitas-cps2 <lucas.freitas@fatec.sp.gov.br>
2025-05-26 12:35:41 -0300

    Script atualizado de acordo com o novo diagrama

--- banco interdisciplinaratt.sql
+++ banco interdisciplinaratt.sql
@@ -1,44 +1,52 @@
-create database BDEduca;
-use BDEduca;
+CREATE DATABASE BDEduca;
+USE BDEduca;
+
+CREATE TABLE funcao (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    funcao VARCHAR(40)
+);
 
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome_usuario VARCHAR(65) NOT NULL,
-    email VARCHAR(255) unique
+    email VARCHAR(255) UNIQUE,
+    senha VARCHAR(255),
+    funcao_id INT,
+    FOREIGN KEY (funcao_id) REFERENCES funcao(id)
 );
 
 CREATE TABLE instituicoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    endereco varchar(300)
+    endereco VARCHAR(300)
 );
-CREATE TABLE cursos (
-id INT auto_increment PRIMARY KEY,
-nome varchar(100) not null,
-instituicao_id INT NOT NULL,
-FOREIGN KEY (instituicao_id) references instituicoes(id)
+
+CREATE TABLE selos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50)
 );
-CREATE TABLE avalia_curso(
- id INT AUTO_INCREMENT PRIMARY KEY,
- usuario_id INT NOT NULL,
- curso_id INT NOT NULL,
- nota INT CHECK (nota BETWEEN 1 AND 5),
- comentario TEXT,
- data_avaliacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
- FOREIGN KEY (curso_id) REFERENCES cursos(id)
+
+CREATE TABLE instituicao_selos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    instituicao_id INT NOT NULL,
+    selo_id INT NOT NULL,
+    FOREIGN KEY (instituicao_id) REFERENCES instituicoes(id),
+    FOREIGN KEY (selo_id) REFERENCES selos(id),
+    UNIQUE (instituicao_id, selo_id)
 );
-CREATE TABLE avalia_instituicao(
-id INT AUTO_INCREMENT PRIMARY KEY,
-usuario_id INT NOT NULL,
-instituicao_id INT NOT NULL,
-nota INT CHECK (nota BETWEEN 1 AND 5),
-comentario TEXT,
- data_avaliacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
- FOREIGN KEY (instituicao_id) REFERENCES instituicoes(id)
+
+CREATE TABLE avalia_instituicao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    instituicao_id INT NOT NULL,
+    nota INT CHECK (nota BETWEEN 1 AND 5),
+    comentario TEXT,
+    data_avaliacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (instituicao_id) REFERENCES instituicoes(id),
+    UNIQUE (usuario_id, instituicao_id)
 );
--- PARA FAZER O RANKING DE MELHOR/PIOR AVALIADO --
+
 CREATE VIEW ranking_instituicoes AS
 SELECT 
     i.id AS instituicao_id,
@@ -53,18 +61,3 @@ GROUP BY
     i.id, i.nome
 ORDER BY 
     media_avaliacoes DESC, total_avaliacoes DESC;
-
-
-
--- MODIFICAÇÕES -- 
-ALTER TABLE usuarios MODIFY COLUMN facebook_id VARCHAR(50);
-ALTER TABLE instituicoes MODIFY COLUMN nome VARCHAR(255) NOT NULL UNIQUE;
-ALTER TABLE usuarios drop column facebook_id;
-ALTER TABLE usuarios drop column google_id;
-select * from usuarios;
-alter table usuarios add column senha varchar(8) not null unique;
-alter table usuarios modify column senha varchar(255) not null;
-drop table avaliacoes;
-DROP VIEW IF EXISTS ranking_instituicoes;
-
-
